243d2f0f2069c7c0eb218f4d31bedba8304ba852
Nicolas CELLIER <eryole@gmail.com>
2017-02-18 13:50:58 +0100

    update tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 import pytest
 from energyplus_wrapper import run
 
@@ -9,5 +10,8 @@ from energyplus_wrapper import run
                                  "8.4.0", "8.5.0",
                                  "8.6.0", "latest"])
 def test_docker_run(tag):
-    run('tests/in.idf',
-        'tests/in.epw', docker_tag=tag)
+    run('tests/in.idf', 'tests/in.epw', docker_tag=tag)
+
+
+def test_local_run():
+    run('tests/in.idf', 'tests/in.epw', docker_tag="")
