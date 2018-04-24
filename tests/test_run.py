79d32d563701a60e922e0a75ea08fb40c4646de7
Nicolas Cellier <contact@nicolas-cellier.net>
2018-04-24 22:36:24 +0200

    allow multiple version testing

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import functools as ft
+import multiprocessing as mp
+
 import pytest
+
 from energyplus_wrapper import run
 
 
-@pytest.mark.parametrize("bin_path",
-                         [None,
-                          "tests/EnergyPlus-8-4-0"])
-def test_run(bin_path):
-    run('tests/in.idf',
+@pytest.mark.parametrize("version",
+                         ["8-4-0",
+                          "8-7-0"])
+def test_run(version):
+    run('tests/in_%s.idf' % version,
         'tests/in.epw',
-        idd_file='tests/Energy+.idd',
-        bin_path=bin_path)
+        idd_file='tests/EnergyPlus-%s/Energy+.idd' % version,
+        bin_path="tests/EnergyPlus-%s/energyplus" % version)
+
+
+def run_mp(i, version):
+    run('tests/in_%s.idf' % version,
+        'tests/in.epw',
+        idd_file='tests/EnergyPlus-%s/Energy+.idd' % version,
+        bin_path="tests/EnergyPlus-%s/energyplus" % version)
+
+
+@pytest.mark.parametrize("version",
+                         ["8-4-0",
+                          "8-7-0"])
+def test_run_mp(version):
+    with mp.Pool() as p:
+        p.map(ft.partial(run_mp, version=version), range(8))
 
 
 if __name__ == '__main__':
@@ -20,4 +39,4 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler())
     logger.setLevel("DEBUG")
-    test_run(None)
+    test_run_mp("8-7-0")
