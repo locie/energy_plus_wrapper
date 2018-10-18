d8cf98eda837a9b66c84bcbae01327752f2996c0
Nicolas CELLIER <eryole@gmail.com>
2018-10-18 14:33:17 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 
-version = '0.6.1'
+version = '0.7.0'
 
 install_requires = ["pandas", "path.py"]
 
